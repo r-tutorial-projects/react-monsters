89cf340ce9e1d78fa3c04e5ed1d033770896d099
Ruben Vitt <ruben.vitt@outlook.de>
2019-12-31 09:04:42 +0100

    Updates

---
+++ precache-manifest.b55dc2807a81e2514680f0f60c6e37f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b6f1ecf05be8accbf882e29528fad63",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "5ba5747e3986f1f8fa3b",
+    "url": "/react-monsters/static/css/main.90f439e3.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/react-monsters/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monsters/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ba5747e3986f1f8fa3b",
+    "url": "/react-monsters/static/js/main.68283053.chunk.js"
+  },
+  {
+    "revision": "3cca4ea8ae7f881f1334",
+    "url": "/react-monsters/static/js/runtime-main.47a36229.js"
+  }
+]);
